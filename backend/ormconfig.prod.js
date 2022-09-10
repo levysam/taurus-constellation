63abeadfc8642fb4a2b479b7c0c3b55362b5e19a
Levy Sampaio <levyaraujo42@gmail.com>
2022-09-10 15:31:28 -0300

    ajustando tsconfig e dockerizando o projeto

---
+++ backend/ormconfig.prod.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    migrations: [
+      './build/database/migrations/**.js',
+    ],
+    entities: [
+      './build/app/domains/*/entities/**.js',
+    ],
+    logging: false,
+    cli: {
+      migrationsDir: './build/database/migrations',
+    },
+  };;
